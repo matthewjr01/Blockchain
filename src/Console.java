92fd803f00b57906d33c624a737826e37f23c15b
matthewjr01 <68038796+matthewjr01@users.noreply.github.com>
2022-03-17 12:44:40 -0700

    Update

--- src/Console.java
+++ src/Console.java
@@ -21,8 +21,12 @@ public class Console extends Thread {
                             System.out.println(Settings.RED+ "UPDATING AND REBOOTING!!!" + Settings.RESET);
                             Process p1 = Runtime.getRuntime().exec(new String[]{"bash","-c","git pull; echo "+"REBOOTING NODE"+"; pkill -9 java; java -jar Blockchain.jar"});
                         case "TEST_ENC":
-                            for(int x = 0; x <= 1000; x++){
-                                System.out.println(StringUtil.applySha256(Integer.toString(x)));
+                            for(int x = 0; x <= 10000000; x++){
+                                if (x >= x%2){
+                                    System.out.println(StringUtil.applySha256(Integer.toString(x)));
+                                }else {
+                                    System.out.println(Settings.GREEN+ StringUtil.applySha512(Integer.toString(x))+ Settings.RESET + "     FOUND!!");
+                                }
                             }
                     }
                 }
