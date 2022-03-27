fb51efb23d292be6414f92cdef1cce84a3e34716
matthewjr01 <68038796+matthewjr01@users.noreply.github.com>
2022-03-26 20:27:53 -0700

    up

--- src/Clients.java
+++ src/Clients.java
@@ -21,8 +21,8 @@ public class Clients extends Thread implements Serializable{
     private ObjectInputStream objectInputStream;
 
 
-    public PrivateKey privateKey;
-    public PublicKey publicKey;
+    public String privateKey;
+    public String publicKey;
     public SecretKey secretKey;
     public Cipher cipher;
 
@@ -38,8 +38,8 @@ public class Clients extends Thread implements Serializable{
 //            generateKeyPair();
             new Notification("Made Client for: "+ socket.getInetAddress(), 4);
 
-            PublicKey pub= (PublicKey) objectInputStream.readObject();
-            PrivateKey privateKey = (PrivateKey) objectInputStream.readObject();
+            String pub= (String) objectInputStream.readObject();
+            String privateKey = (String) objectInputStream.readObject();
 
             publicKey = pub;
             if(!Networking.Network_Clients_Identifiers.contains(StringUtil.applySha256(publicKey.toString()))){
@@ -144,24 +144,24 @@ public class Clients extends Thread implements Serializable{
     }
 
 
-    public void generateKeyPair() {
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-
-        try {
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
-            // Initialize the key generator and generate a KeyPair
-            keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
-            KeyPair keyPair = keyGen.generateKeyPair();
-            // Set the public and private keys from the keyPair
-
-            publicKey = keyPair.getPublic();
-            privateKey = keyPair.getPrivate();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public void generateKeyPair() {
+//        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+//
+//        try {
+//            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+//            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+//            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+//            // Initialize the key generator and generate a KeyPair
+//            keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+//            KeyPair keyPair = keyGen.generateKeyPair();
+//            // Set the public and private keys from the keyPair
+//
+//            publicKey = keyPair.getPublic();
+//            privateKey = keyPair.getPrivate();
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
